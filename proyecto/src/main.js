d707e71d1c201250bfe72ee753ab2c6c3013ca25
unknown <CIAC@CIAC2-004.academic.mixcoac.upmx.mx>
2023-05-19 11:31:44 -0600

    Monitoreo claves y claves nuevas

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -23,8 +23,31 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+///////////////////////// FIREBASE /////////////////////////////////////
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnr9APkry3XeB9NF69dvGkkexeqa5qTI0",
+  authDomain: "proyecto-b55ad.firebaseapp.com",
+  projectId: "proyecto-b55ad",
+  storageBucket: "proyecto-b55ad.appspot.com",
+  messagingSenderId: "307174819683",
+  appId: "1:307174819683:web:30e46fc76241bd0e86f556"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+////////////////////////////////////////////////////////////////////////
+
 const app = createApp(App)
   .use(IonicVue)
+  .use(appFirebase)
   .use(router);
   
 router.isReady().then(() => {
