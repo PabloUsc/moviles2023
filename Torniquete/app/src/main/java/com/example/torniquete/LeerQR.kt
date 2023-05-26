fa67da7f848f7a3acb09383367847e924e463806
Pablo Uscanga <0225717@up.edu.mx>
2023-05-25 23:47:16 -0600

    Lector QR completo

--- Torniquete/app/src/main/java/com/example/torniquete/LeerQR.kt
+++ Torniquete/app/src/main/java/com/example/torniquete/LeerQR.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import com.example.torniquete.databinding.ActivityLeerqrBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlin.concurrent.schedule
+import java.util.Timer
 
 class LeerQR  : AppCompatActivity() {
 
     private lateinit var binding: ActivityLeerqrBinding
+    private lateinit var auth: FirebaseAuth;
+
+    val database = Firebase.database
+    val myRef = database.getReference("claves")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_leerqr)
@@ -38,10 +49,40 @@ class LeerQR  : AppCompatActivity() {
         {
             if (result.contents == null)
             {
-                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "No se encontró código QR", Toast.LENGTH_LONG).show()
             }
-            else { binding.resultQR.text=result.contents.toString() }
+            else {
+                //Inserte el leer el nodo
+                myRef.child(result.contents.toString()).child("status").get().addOnSuccessListener {
+                    //Log.i("firebase", "Got value ${it.value}")
+                    var res = it.value.toString()
+                    Toast.makeText(this, res, Toast.LENGTH_LONG).show()
+                    if (res == "Generado") {
+                        myRef.child(result.contents.toString()).child("status").setValue("Utilizado")
+                        binding.resultQR.text = "¡Qué tenga un buen viaje!"
+                        Timer().schedule(6000) {
+                            binding.resultQR.text = ""
+                        }
+                    } else {
+                        binding.resultQR.text = "QR utilizado. Genere un nuevo QR para ingresar"
+                        Timer().schedule(8000) {
+                            binding.resultQR.text = ""
+                        }
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(this, "Hubo un error en la lectura del QR", Toast.LENGTH_LONG).show()
+                }
+                //var res = myRef.child(result.contents.toString()).child("status")
+                //Asignación de valor "utilizado" al nodo
+                //myRef.child(result.contents.toString()).child("status").setValue("Utilizado")
+                //Toast.makeText(this, res, Toast.LENGTH_LONG).show()
+            }
         }
         else { super.onActivityResult(requestCode, resultCode, data) }
     }//*/
+    fun logOut() {
+        auth.signOut()
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
 }
